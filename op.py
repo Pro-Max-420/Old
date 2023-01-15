6175ebf551720d9e2a457ede715f87e2723b50a4
Pro-Max-420 <121747358+Pro-Max-420@users.noreply.github.com>
2023-01-15 10:32:52 +0600

    op.py

--- op.py
+++ op.py
@@ -1,5 +1,6 @@
 import os,time
-os.system("xdg-open https://youtube.com/channel/UCbT--Z1XzQpSUgjD6bfzzUA")
+print('\033[1;34m\n Congratulations! Your Device Support This Tools\033[1;37m')
+    if path.isfile("XD.so"):
 time.sleep(1)
 import op
 op.main()
